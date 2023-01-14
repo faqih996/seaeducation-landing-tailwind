7563a421759b95c5fbbea46cea24d8c74d445e08
Faqih996 <faqih.syakir11@gmail.com>
2023-01-15 00:22:37 +0700

    Make Slider For Header

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -12,3 +12,26 @@ accordionHeader.forEach(accordionHeader => {
         }
     })
 })
+
+//swiper JS
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
